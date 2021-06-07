9fea16952b668540b079698135d90e71ac15cd2b
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2021-06-07 16:12:39 +0530

    static leaderboard

--- server/routes/play.py
+++ server/routes/play.py
@@ -1,13 +1,13 @@
+from flask import send_from_directory
 from server.api_handlers import play
 from server.app_init import app
 
 from server.util import ParsedRequest, api_response
 
 
-@app.get("/play/<event>/leaderboard/", strict_slashes=False)
-@api_response
-def leaderboard(event):
-    return play.leaderboard(event)
+@app.get("/play/main/leaderboard/", strict_slashes=False)
+def leaderboard():
+    return send_from_directory("static", "leaderboard.json")
 
 
 @app.get("/play/<event>/question/", strict_slashes=False)
